4bc12130b9fd6ccdc01cb4502fbcdfc977fd0bdb
Ashwin J <63511472+Ashwin20102000@users.noreply.github.com>
2021-06-15 14:17:04 +0530

    Create DuplicateNumber.cpp

---
+++ LeetCode/DuplicateNumber.cpp
@@ -0,0 +1,31 @@
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and uses only constant extra space.
+
+Example 1:
+
+Input: nums = [1,3,4,2,2]
+Output: 2
+Example 2:
+
+Input: nums = [3,1,3,4,2]
+Output: 3
+Example 3:
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+            if(m[nums[i]]>1)return nums[i];
+        }
+        return -1;
+    }
+};
+
+
+
+https://leetcode.com/problems/find-the-duplicate-number/
