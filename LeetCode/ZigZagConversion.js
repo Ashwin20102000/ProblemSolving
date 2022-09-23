223e62f3977104b229d47bb80d630f84a23728ea
Ashwin J <63511472+Ashwin20102000@users.noreply.github.com>
2022-09-24 02:02:06 +0530

    Create ZigZagConversion.js

---
+++ LeetCode/ZigZagConversion.js
@@ -0,0 +1,60 @@
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string s, int numRows);
+ 
+
+Example 1:
+
+Input: s = "PAYPALISHIRING", numRows = 3
+Output: "PAHNAPLSIIGYIR"
+Example 2:
+
+Input: s = "PAYPALISHIRING", numRows = 4
+Output: "PINALSIGYAHRPI"
+Explanation:
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+Example 3:
+
+Input: s = "A", numRows = 1
+Output: "A"
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+    if (numRows === 1) {
+        return s;
+    }
+	let arr=[],idx=0,move=0;
+    arr.length=numRows;
+    arr.fill("");
+    for(let i=0;i<s.length;i++){
+        arr[idx]+=s[i];
+        if(idx===0){
+            move=1;
+        }
+        else if(idx===(numRows-1)){
+            move=-1;
+        }
+        idx+=move;
+    }
+    return arr.join("");
+    
+};
+
+
+// Success
+// Details 
+// Runtime: 100 ms, faster than 87.22% of JavaScript online submissions for Zigzag Conversion.
+// Memory Usage: 45.3 MB, less than 95.08% of JavaScript online submissions for Zigzag Conversion.
