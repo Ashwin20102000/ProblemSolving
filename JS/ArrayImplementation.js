f0a724af989b5e9524cc9b36b358571c3fa7e103
Ashwin J <63511472+Ashwin20102000@users.noreply.github.com>
2022-09-27 14:52:57 +0530

    Update ArrayImplementation.js

--- JS/ArrayImplementation.js
+++ JS/ArrayImplementation.js
@@ -21,17 +21,65 @@ class Arr {
         return this.arr;
     }
     deleteAt(idx){
-        
+        const pop = this.arr[idx];
         for(let i=idx;i<this.length;i++){
             this.arr[i]=this.arr[i+1];
         }
         this.arr.length = --this.length;
-        return this.arr;
+        return pop;
     }
     getElementAt(idx){
         return this.arr[idx];
     }
     show(){
+        return this.arr;
+    }
+    
+    
+    //  Js Methods 
+    toString(){
+        return this.arr+"";
+    }
+    join(exp=","){
+        let string = this.arr[0]+exp;
+        for(let i=1;i<this.length-1;i++){
+            string+=(this.arr[i]+exp);
+        }
+        string+=this.arr[this.length-1];
+        return string;
+    }
+    
+    shift(){
+        for(let i=1;i<this.length;i++){
+            this.arr[i-1]=this.arr[i];
+        }
+        this.arr.length = --this.length;
+        return this.arr;
+    }
+    
+    unshift(data){
+        if(Array.isArray(data)){
+            this.arr = [...data,...this.arr];
+        }
+        else{
+            this.arr=[data,...this.arr];
+            this.length++;
+        }
+        return this.arr;
+    }
+    
+    len(){
+        return this.length;
+    }
+    
+    concat(data){
+        if(Array.isArray(data)){
+            this.arr = [...this.arr,...data];
+        }
+        else{
+            this.arr=[...this.arr,data];
+        }
+        this.length=this.arr.length;
         return this.arr;
     }
 }
@@ -42,8 +90,16 @@ arr.push(25);
 arr.push(26);
 arr.push(27);
 arr.push(29);
+arr.push(39);
 arr.pop();
 arr.insertAt(2,12)
 console.log(arr.show());
 arr.deleteAt(3)
 console.log(arr.getElementAt(1));
+console.log(arr.toString());
+console.log(arr.join());
+console.log(arr.shift());
+console.log(arr.unshift(31));
+console.log(arr.len());
+console.log(arr.concat(32));
+console.log(arr.show());
