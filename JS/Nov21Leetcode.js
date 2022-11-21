5ae79c06a2587b0a918b3c2ee1f0c6fac9c1acac
Ashwin J <63511472+Ashwin20102000@users.noreply.github.com>
2022-11-21 17:15:47 +0530

    Update Nov21Leetcode.js

--- JS/Nov21Leetcode.js
+++ JS/Nov21Leetcode.js
@@ -71,7 +71,70 @@ var removeSubfolders = function(folder) {
 // Beats
 // 90.48%
 // Status
+// // Success
+
+// 28. Find the Index of the First Occurrence in a String
+// Medium
+// 884
+// 71
+// Companies
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+ 
+
+// Example 1:
+
+// Input: haystack = "sadbutsad", needle = "sad"
+// Output: 0
+// Explanation: "sad" occurs at index 0 and 6.
+// The first occurrence is at index 0, so we return 0.
+// Example 2:
+
+// Input: haystack = "leetcode", needle = "leeto"
+// Output: -1
+// Explanation: "leeto" did not occur in "leetcode", so we return -1.
+ 
+
+// Constraints:
+
+// 1 <= haystack.length, needle.length <= 104
+// haystack and needle consist of only lowercase English characters.
+// Accepted
+// 1.5M
+// Submissions
+// 3.9M
+// Acceptance Rate
+// 37.6%
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    let ans = -1;
+    for(let i=0;i<haystack.length;i++){
+        if(haystack[i]===needle[0] && haystack.slice(i,i+needle.length)===needle){
+            ans=i;
+            break;
+        }
+    }
+    return ans;
+};
+
+// JavaScript
+// Runtime
+// 92 ms
+// Beats
+// 60.27%
+// Memory
+// 42 MB
+// Beats
+// 64.14%
+// Status
 // Success
+
+
 
 // 80. Remove Duplicates from Sorted Array II
 // Medium
@@ -297,6 +360,91 @@ var search = function(nums, target) {
 // 42.8 MB
 // Beats
 // 33.86%
+// Status
+// Success
+
+// 27. Remove Element
+// Easy
+// 4.8K
+// 6.5K
+// Companies
+// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
+
+// Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+// Return k after placing the final result in the first k slots of nums.
+
+// Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+// Custom Judge:
+
+// The judge will test your solution with the following code:
+
+// int[] nums = [...]; // Input array
+// int val = ...; // Value to remove
+// int[] expectedNums = [...]; // The expected answer with correct length.
+//                             // It is sorted with no values equaling val.
+
+// int k = removeElement(nums, val); // Calls your implementation
+
+// assert k == expectedNums.length;
+// sort(nums, 0, k); // Sort the first k elements of nums
+// for (int i = 0; i < actualLength; i++) {
+//     assert nums[i] == expectedNums[i];
+// }
+// If all assertions pass, then your solution will be accepted.
+
+ 
+
+// Example 1:
+
+// Input: nums = [3,2,2,3], val = 3
+// Output: 2, nums = [2,2,_,_]
+// Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+// It does not matter what you leave beyond the returned k (hence they are underscores).
+// Example 2:
+
+// Input: nums = [0,1,2,2,3,0,4,2], val = 2
+// Output: 5, nums = [0,1,4,0,3,_,_,_]
+// Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+// Note that the five elements can be returned in any order.
+// It does not matter what you leave beyond the returned k (hence they are underscores).
+ 
+
+// Constraints:
+
+// 0 <= nums.length <= 100
+// 0 <= nums[i] <= 50
+// 0 <= val <= 100
+// Accepted
+// 1.6M
+// Submissions
+// 3M
+// Acceptance Rate
+// 52.2%
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let n = nums.length;
+    while(n--)(nums[n]===val) && nums.splice(n,1);
+    return nums.length;
+};
+
+
+// JavaScript
+// Runtime
+// 100 ms
+// Beats
+// 42.45%
+// Memory
+// 42.4 MB
+// Beats
+// 11.85%
 // Status
 // Success
 
